3a81416a81d9343d0e5908f37d897514fada8139
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2025-03-11 17:23:44 +0800

    bugfix for authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,38 @@
-import userModel from "../models/userModel.js";
-import orderModel from "../models/orderModel.js";
+import userModel from '../models/userModel.js';
+import orderModel from '../models/orderModel.js';
 
-import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
-import JWT from "jsonwebtoken";
+import { comparePassword, hashPassword } from './../helpers/authHelper.js';
+import JWT from 'jsonwebtoken';
 
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address, answer } = req.body;
     //validations
     if (!name) {
-      return res.status(400).send({ error: "Name is Required" });
+      return res.status(400).send({ error: 'Name is Required' });
     }
     if (!email) {
-      return res.status(400).send({ message: "Email is Required" });
+      return res.status(400).send({ message: 'Email is Required' });
     }
     if (!password) {
-      return res.status(400).send({ message: "Password is Required" });
+      return res.status(400).send({ message: 'Password is Required' });
     }
     if (!phone) {
-      return res.status(400).send({ message: "Phone no is Required" });
+      return res.status(400).send({ message: 'Phone no is Required' });
     }
     if (!address) {
-      return res.status(400).send({ message: "Address is Required" });
+      return res.status(400).send({ message: 'Address is Required' });
     }
     if (!answer) {
-      return res.status(400).send({ message: "Answer is Required" });
+      return res.status(400).send({ message: 'Answer is Required' });
     }
 
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g.test(email) === false) {
-      return res.status(400).send({ message: "Invalid Email" });
+    if (
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g.test(
+        email
+      ) === false
+    ) {
+      return res.status(400).send({ message: 'Invalid Email' });
     }
 
     //check user
@@ -37,7 +41,7 @@ export const registerController = async (req, res) => {
     if (exisitingUser) {
       return res.status(200).send({
         success: false,
-        message: "Already Register please login",
+        message: 'Already Register please login',
       });
     }
     //register user
@@ -54,14 +58,14 @@ export const registerController = async (req, res) => {
 
     res.status(201).send({
       success: true,
-      message: "User Register Successfully",
+      message: 'User Register Successfully',
       user,
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Errro in Registeration",
+      message: 'Errro in Registeration',
       error,
     });
   }
@@ -75,7 +79,7 @@ export const loginController = async (req, res) => {
     if (!email || !password) {
       return res.status(404).send({
         success: false,
-        message: "Invalid email or password",
+        message: 'Invalid email or password',
       });
     }
     //check user
@@ -83,23 +87,23 @@ export const loginController = async (req, res) => {
     if (!user) {
       return res.status(404).send({
         success: false,
-        message: "Email is not registerd",
+        message: 'Email is not registerd',
       });
     }
     const match = await comparePassword(password, user.password);
     if (!match) {
       return res.status(400).send({
         success: false,
-        message: "Invalid Password",
+        message: 'Invalid Password',
       });
     }
     //token
     const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: '7d',
     });
     res.status(200).send({
       success: true,
-      message: "login successfully",
+      message: 'login successfully',
       user: {
         _id: user._id,
         name: user.name,
@@ -114,7 +118,7 @@ export const loginController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error in login",
+      message: 'Error in login',
       error,
     });
   }
@@ -126,13 +130,13 @@ export const forgotPasswordController = async (req, res) => {
   try {
     const { email, answer, newPassword } = req.body;
     if (!email) {
-      res.status(400).send({ message: "Emai is required" });
+      res.status(400).send({ message: 'Emai is required' });
     }
     if (!answer) {
-      res.status(400).send({ message: "answer is required" });
+      res.status(400).send({ message: 'answer is required' });
     }
     if (!newPassword) {
-      res.status(400).send({ message: "New Password is required" });
+      res.status(400).send({ message: 'New Password is required' });
     }
     //check
     const user = await userModel.findOne({ email, answer });
@@ -140,20 +144,20 @@ export const forgotPasswordController = async (req, res) => {
     if (!user) {
       return res.status(404).send({
         success: false,
-        message: "Wrong Email Or Answer",
+        message: 'Wrong Email Or Answer',
       });
     }
     const hashed = await hashPassword(newPassword);
     await userModel.findByIdAndUpdate(user._id, { password: hashed });
     res.status(200).send({
       success: true,
-      message: "Password Reset Successfully",
+      message: 'Password Reset Successfully',
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Something went wrong",
+      message: 'Something went wrong',
       error,
     });
   }
@@ -162,7 +166,7 @@ export const forgotPasswordController = async (req, res) => {
 //test controller
 export const testController = (req, res) => {
   try {
-    res.send("Protected Routes");
+    res.send('Protected Routes');
   } catch (error) {
     console.log(error);
     res.send({ error });
@@ -175,11 +179,11 @@ export const updateProfileController = async (req, res) => {
     const { name, email, password, address, phone } = req.body;
     const user = await userModel.findById(req.user._id);
     if (!user) {
-      return res.status(404).send({ message: "User Not Found" });
+      return res.status(404).send({ message: 'User Not Found' });
     }
     //password
     if (password && password.length < 6) {
-      return res.json({ error: "Passsword is required and 6 character long" });
+      return res.json({ error: 'Passsword is required and 6 character long' });
     }
     const hashedPassword = password ? await hashPassword(password) : undefined;
     const updatedUser = await userModel.findByIdAndUpdate(
@@ -194,14 +198,14 @@ export const updateProfileController = async (req, res) => {
     );
     res.status(200).send({
       success: true,
-      message: "Profile Updated SUccessfully",
+      message: 'Profile Updated SUccessfully',
       updatedUser,
     });
   } catch (error) {
     console.log(error);
     res.status(400).send({
       success: false,
-      message: "Error WHile Update profile",
+      message: 'Error WHile Update profile',
       error,
     });
   }
@@ -212,14 +216,14 @@ export const getOrdersController = async (req, res) => {
   try {
     const orders = await orderModel
       .find({ buyer: req.user._id })
-      .populate("products", "-photo")
-      .populate("buyer", "name");
+      .populate('products', '-photo')
+      .populate('buyer', 'name');
     res.json(orders);
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error WHile Geting Orders",
+      message: 'Error WHile Geting Orders',
       error,
     });
   }
@@ -229,15 +233,15 @@ export const getAllOrdersController = async (req, res) => {
   try {
     const orders = await orderModel
       .find({})
-      .populate("products", "-photo")
-      .populate("buyer", "name")
-      .sort({ createdAt: "-1" });
+      .populate('products', '-photo')
+      .populate('buyer', 'name')
+      .sort({ createdAt: -1 });
     res.json(orders);
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error WHile Geting Orders",
+      message: 'Error WHile Geting Orders',
       error,
     });
   }
@@ -258,7 +262,7 @@ export const orderStatusController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error While Updateing Order",
+      message: 'Error While Updateing Order',
       error,
     });
   }
